6d5cd1cec69accf741d75057486e6c09f8c32c1a
raingang <raingang4@gmail.com>
2018-05-30 14:28:37 +0300

    Anton_Abrosymov Add tests for UserTable

---
+++ src/__tests__/UsersTable.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Route } from 'react-router';
+
+import UsersTable from '../components/UsersTable';
+import Spinner from '../components/Spinner';
+import App from '../components/App';
+
+
+describe('<UsersTable', () => {
+
+	it('Render UserTable', () => {
+		const wrapper = shallow(<App logged = {true} />);
+		expect(wrapper
+			.find(Route)
+			.find({ path: '/users' })
+		).toHaveLength(1);
+	});
+
+	it('Render User', () => {
+		const wrapper = shallow(<UsersTable users = {[
+			{
+				"firstName": "test",
+				"lastName": "test",
+				"age": 1,
+				"visits": 0,
+				"progress": 0,
+				"status": "relationship",
+				"id": 74
+			},
+		]}/>);
+		expect(wrapper.find('tbody').children()).toHaveLength(1);
+	});
+
+	it('Render Spinner', () => {
+		const wrapper = shallow(<UsersTable users = {[]}/>);
+		expect(wrapper.contains(<Spinner />)).toBeTruthy;
+	})
+
+});
---
+++ src/__tests__/test.js
@@ -0,0 +1,5 @@
+describe('Jest works', () => {
+	it('should equal 4', () => {
+		expect(2+2).toBe(4);
+	});
+});
