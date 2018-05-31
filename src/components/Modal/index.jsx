6dad8a01fde2742c1f4e5a8a91588834e35224a1
raingang <raingang4@gmail.com>
2018-05-31 12:36:13 +0300

    Anton_Abrosymov Lint code

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -5,28 +5,30 @@ import UserForm from '../../containers/UserForm';
 import '../../styles/Modal/index.css';
 
 class Modal extends Component {
+
   onClose = () => {
       this.props.toggleModal();
-  };
-    render(){
-      return(
-        <div className="modal" tabIndex="-1" role="dialog">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Add new user</h5>
-                <button onClick = { this.onClose }type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <UserForm handleClose = {this.props.toggleModal} handleAction = {this.props.handleAddUser}>Add</UserForm>
-              </div>
+  }
+
+  render(){
+    return(
+      <div className="modal" tabIndex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Add new user</h5>
+              <button onClick = { this.onClose }type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <UserForm handleClose = {this.props.toggleModal} handleAction = {this.props.handleAddUser}>Add</UserForm>
             </div>
           </div>
         </div>
-      )
-    };
+      </div>
+    )
+  };
 }
 
 Modal.propTypes = {
